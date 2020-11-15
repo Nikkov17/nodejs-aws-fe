3a6b02576a3b9a0f5e1004e09eac960774bd1ca8
kolya <kovalyov.k51@gmail.com>
2020-11-16 00:12:58 +0300

    [TASK-5] change import url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://9q25ctsu4k.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://9q25ctsu4k.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://9q25ctsu4k.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://th8mexnih6.execute-api.eu-west-1.amazonaws.com/dev/import",
   bff: "https://9q25ctsu4k.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
